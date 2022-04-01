13762cc89cf7e82d6f4003a48c18ff44784989b7
RenoPles <102758784+RenoPles@users.noreply.github.com>
2022-04-01 13:53:19 -0400

    Update HexCipher.py

--- HexCipher/HexCipher/HexCipher.py
+++ HexCipher/HexCipher/HexCipher.py
@@ -1,48 +1,52 @@
-#       Program:    Hex Cipher
-#       Author:     Trent Weikel
-#       Date:       03/29/22
-#       Purpose:    Encrypts or decrypts the users text by using unicode and adding 7 to the hex value.
 
-#       Test Cases: 
-#                   Hello, World! = 4F-6C-73-73-76-33-27-5E-76-79-73-6B-28
-#                   ☺☻♥♦♣♠• = 2641-2642-266C-266D-266A-2667-2029
+def main():
+    #       Program:    Hex Cipher
+    #       Author:     Trent Weikel
+    #       Date:       03/29/22
+    #       Purpose:    Encrypts or decrypts the users text by using unicode and adding 7 to the hex value.
 
-#######################################################################
-#                                                                     #
-#                              Imports                                #
-#                                                                     #
-#######################################################################
+    #       Test Cases: 
+    #                   Hello, World! = 4F-6C-73-73-76-33-27-5E-76-79-73-6B-28
+    #                   ☺☻♥♦♣♠• = 2641-2642-266C-266D-266A-2667-2029
 
-import Functions    # Imports the Functions.py file.
+    #######################################################################
+    #                                                                     #
+    #                              Imports                                #
+    #                                                                     #
+    #######################################################################
 
-#######################################################################
-#                                                                     #
-#                               Code                                  #
-#                                                                     #
-#######################################################################
-    
-encrypt_or_decrypt = ""
-continue_script = False
+    import Functions    # Imports the Functions.py file.
 
-while (continue_script == False):
+    #######################################################################
+    #                                                                     #
+    #                               Code                                  #
+    #                                                                     #
+    #######################################################################
 
-    encrypt_or_decrypt = input("Encrypting or Decrypting (E/D): ").upper()
+    encrypt_or_decrypt = ""
+    continue_script = False
 
-    if (encrypt_or_decrypt.find("E") != -1 and len(encrypt_or_decrypt) == 1): continue_script = True
-    elif (encrypt_or_decrypt.find("D") != -1 and len(encrypt_or_decrypt) == 1): continue_script = True
-    else: print("\nPlease enter a valid character! (e/d)\n")
-    # Checks if variable is a valid value to rerun loop if needed.
+    while (continue_script == False):
 
-# end while
-    
-if (encrypt_or_decrypt.find("E") != -1):
+        encrypt_or_decrypt = input("Encrypting or Decrypting (E/D): ").upper()
 
-    user_input = input("Message To Encrypt: ")      # Gets the message the user wants to encrypt.
-    print(f"Encrypted Message: {Functions.encrypt(user_input)}\n")
+        if (encrypt_or_decrypt.find("E") != -1 and len(encrypt_or_decrypt) == 1): continue_script = True
+        elif (encrypt_or_decrypt.find("D") != -1 and len(encrypt_or_decrypt) == 1): continue_script = True
+        else: print("\nPlease enter a valid character! (e/d)\n")
+        # Checks if variable is a valid value to rerun loop if needed.
 
-else:
+    # end while
 
-    user_input = input("Message To Decrypt: ")      # Gets the message the user wants to decrypt.
-    print(f"\nDecrypted Message: {Functions.decrypt(user_input)}\n")
-    
-# end if
+    if (encrypt_or_decrypt.find("E") != -1):
+
+        user_input = input("Message To Encrypt: ")      # Gets the message the user wants to encrypt.
+        print(f"Encrypted Message: {Functions.encrypt(user_input)}\n")
+
+    else:
+
+        user_input = input("Message To Decrypt: ")      # Gets the message the user wants to decrypt.
+        print(f"\nDecrypted Message: {Functions.decrypt(user_input)}\n")
+
+    # end if
+if __name__ == "__main__":
+    main()
